491f29533d9e73385566c71a3095af4d0b38a270
red19958 <75182860+red19958@users.noreply.github.com>
2021-11-27 14:38:06 +0300

    Create upback.bash

---
+++ upback.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /root
+if [[ ! -d restore ]]
+then 
+	mkdir restore
+fi
+
+back="$(ls | grep -E "^Backup-" | sort -V | tail -1 )"
+cd /root/$back
+files=$(ls)
+for i in $files
+do
+	notFresh=$(echo "$i" | grep -E -o '^[A-Za-z0-9]+-[0-9]+-[0-9]+-[0-9]+')
+	if [ "$i" != "$notFresh" ]
+	then
+		cp -R $i /root/restore
+	fi
+done
