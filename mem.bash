fb3e53c72b74037ecae8124aae89b804a7a18084
red19958 <75182860+red19958@users.noreply.github.com>
2021-12-18 16:48:43 +0300

    Update mem.bash

--- mem.bash
+++ mem.bash
@@ -6,10 +6,10 @@ echo "" > report.log
 while true
 do
 	array+=(1 2 3 4 5)
-	if [[ "$iter" -eq "100000" ]]
+	if [[ $iter -eq 100000 ]]
 	then
 		iter=0
 		echo ${#array[*]} >> report.log
 	fi
-	let "iter=$iter+1"
+	let iter=$iter+1
 done
